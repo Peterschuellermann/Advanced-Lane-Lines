5b517ca6f8284d88ccb7f5b9c8cfb59e106baace
Peter Schüllermann <Peter.schuellermann>
2017-05-15 18:16:19 +0200

    add camera calibration

--- lanelines.py
+++ lanelines.py
@@ -1,7 +1,50 @@
 # Peter Schüllermann
 # Udacity Lane Lines Project for the CarND 2017
+from moviepy.editor import VideoFileClip
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import glob
+import math # for slope calculations
+
 
 # Camera Calibration
+
+images = glob.glob("camera_cal/calibration*.jpg")
+
+
+def calibration(images):
+    imgpoints = []
+    objpoints = []
+
+    objp = np.zeros((9 * 6, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)  # x, y coordinates
+
+    for fname in images:
+        img = mpimg.imread(fname)
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+
+        if ret == True:
+            imgpoints.append(corners)
+            objpoints.append(objp)
+            print(".")
+            # Draw and display the corners
+            # img = cv2.drawChessboardCorners(img, (8, 6), corners, ret)
+
+    return cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+
+ret, mtx, dist, rvecs, tvecs = calibration(images)
+
+image = cv2.cvtColor(mpimg.imread(images[0]), cv2.COLOR_BGR2GRAY)
+dst = cv2.undistort(image, mtx, dist, None, mtx)
+cv2.imwrite("undistorted.jpg", dst)
+cv2.imwrite("distorted.jpg", image)
+
 
 # Distortion Correction
 
