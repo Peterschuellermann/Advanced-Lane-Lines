cac154642d84df491ec62f000c72853ab04cf67c
Peter Schüllermann <Peter.schuellermann>
2017-05-15 16:40:12 +0200

    add lanelines.py with structure for project

---
+++ lanelines.py
@@ -0,0 +1,16 @@
+# Peter Schüllermann
+# Udacity Lane Lines Project for the CarND 2017
+
+# Camera Calibration
+
+# Distortion Correction
+
+# Perspective Transform
+
+# Detect Lane Pixels
+
+# Determine Curvature
+
+# Paint Curvature onto the original image
+
+# Calculate estimation of the curve radius and print to video
